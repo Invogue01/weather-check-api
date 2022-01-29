fb6eab700237661c287d1b6f14b4eb85b572aa8e
Andrea Mac <aleatrengrouse@gmail.com>
2022-01-29 21:53:58 +0000

    not fetching data trying to fix

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,24 @@
-const get=new Fetch();
-const ui=new UI();
+const ft = new Fetch();
+const ui = new UI();
 
-//event listeners
+//add event listeners//
 
-const search=document.getElementById("searchUser");
-const button=document.getElementById("submit");
+const search = document.getElementById("searchUser");
+const button = document.getElementById("submit");
+button.addEventListener("click", () => {
+  const currentVal = search.value;
+
+  ft.getCurrent(currentVal).then((data) => {
+    //call a UI method//
+    ui.populateUI(data);
+    //call saveToLS
+    ui.saveToLS(data);
+  });
+});
+
+//event listener for local storage
+
+window.addEventListener("DOMContentLoaded", () => {
+  const dataSaved = ui.getFromLS();
+  ui.populateUI(dataSaved);
+});
