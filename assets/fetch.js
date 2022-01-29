23f3f71f8715d25c8b14ff59a86891bb11ccc765
Andrea Mac <aleatrengrouse@gmail.com>
2022-01-29 21:33:30 +0000

    fetch function using api key

---
+++ assets/fetch.js
@@ -0,0 +1,13 @@
+class Fetch {
+    async getCurrent(input){
+        const weatherKey= '37da6e19f9f8ba43dc800c317b9f7bd8';
+
+        //make request to website
+        const response = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${weatherKey}`
+        );
+        const data= await response.json();
+        console.log(data);
+        return data;
+    }
+}
