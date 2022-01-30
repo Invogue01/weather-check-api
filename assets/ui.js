62fc81b3d47d8161ce88bf8774e2482bfcdb1531
Andrea Mac <aleatrengrouse@gmail.com>
2022-01-30 00:00:16 +0000

    js files not linking correctly

--- assets/ui.js
+++ assets/ui.js
@@ -14,8 +14,8 @@ class UI {
           
           <div class="card mx-auto mt-5" style="width: 18rem;">
               <div class="card-body justify-content-center">
-                  <h5 class="card-title text-muted">${data.name} Today</h5>
-                  <h6 class="card-subtitle mb-2 text-muted">Highs of ${data.main.temp_max}. Lows of ${data.main.temp_min} </h6>
+                  <h5 class="card-title text-muted">${data.name}</h5>
+                  <h6 class="card-subtitle mb-2 text-muted">TODAY Highs of ${data.main.temp_max}. Lows of ${data.main.temp_min} </h6>
                 
                   <p class="card-text ">Weather conditions : ${data.weather[0].description}</p>
 
